a9e365bba8ef65dee1df96caee13a39f03061fba
peng fu <fupenglxy@gmail.com>
2018-11-30 02:12:47 +0800

    fix build error

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -1,9 +1,12 @@
 const StylelintWebpackPlugin = require('stylelint-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const MiniProgramWebpackPlugin = require('miniprogram-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const webpack = require('webpack');
+const path = require('path');
 const utils = require('./utils');
 const config = require('../config');
+
+const srcDir = utils.resolve(path.parse(config.entry).dir);
 
 module.exports = {
 	entry: {
@@ -17,9 +20,9 @@ module.exports = {
 	devtool: 'none',
 	resolve: {
 		extensions: ['.js', '.ts'],
-		modules: [utils.resolve('src'), 'node_modules'],
+		modules: [srcDir, 'node_modules'],
 		alias: Object.assign({}, config.alias, {
-			'@': utils.resolve('src'),
+			'@': srcDir,
 		}),
 		symlinks: false
 	},
@@ -57,10 +60,12 @@ module.exports = {
 				exclude: /node_modules/,
 				use: [
 					{
-						loader: MiniCssExtractPlugin.loader
-					},
-					{
-						loader: 'css-loader'
+						loader: 'file-loader',
+						options: {
+							useRelativePath: true,
+							name: `[name].wxss`,
+							context: srcDir,
+						},
 					},
 					{
 						loader: 'postcss-loader'
@@ -87,9 +92,15 @@ module.exports = {
 	},
 
 	plugins: [
-		new MiniCssExtractPlugin({
-			filename: '[name].wxss'
-		}),
+		new CopyWebpackPlugin(
+			[{
+				from: './',
+				to: './'
+			}], {
+				ignore: ['*.js', '*.css', '*.ts', '*.scss', '*.less', '*.sass', '*.wxss', '.wxml'],
+				context: srcDir,
+			}
+		),
 		new MiniProgramWebpackPlugin(),
 		new webpack.optimize.ModuleConcatenationPlugin(),
 		new StylelintWebpackPlugin()
